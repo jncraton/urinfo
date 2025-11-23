5db9f1692a9113035642e0edc9c031f18c94e341
Jon Craton <jncraton@gmail.com>
2025-11-23 15:07:51 -0500

    Include original ISBN

--- urinfo.js
+++ urinfo.js
@@ -1,12 +1,14 @@
 const urinfo = async uri => {
   if (uri.startsWith('urn:isbn:')) {
-    const res = await fetch(`https://openlibrary.org/search.json?q=isbn:${uri.slice(9)}`)
+    const isbn = uri.slice(9)
+    const res = await fetch(`https://openlibrary.org/search.json?q=isbn:${isbn}`)
     const result = await res.json()
 
     return {
       title: result.docs[0].title,
       authors: result.docs[0].author_name,
       year: result.docs[0].first_publish_year,
+      isbn: isbn,
     }
   }
 }
