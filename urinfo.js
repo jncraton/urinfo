8102f16a916488f3821202e6d51c5b471dff82f3
Jon Craton <jncraton@gmail.com>
2025-11-23 15:06:54 -0500

    Reformat

--- urinfo.js
+++ urinfo.js
@@ -1,14 +1,12 @@
-const urinfo = async (uri) => {
-  if (uri.startsWith("urn:isbn:")) {
-    const res = await fetch(
-      `https://openlibrary.org/search.json?q=isbn:${uri.slice(9)}`,
-    );
-    const result = await res.json();
+const urinfo = async uri => {
+  if (uri.startsWith('urn:isbn:')) {
+    const res = await fetch(`https://openlibrary.org/search.json?q=isbn:${uri.slice(9)}`)
+    const result = await res.json()
 
     return {
-      'title': result.docs[0].title,
-      'authors': result.docs[0].author_name,
-      'year': result.docs[0].first_publish_year,
+      title: result.docs[0].title,
+      authors: result.docs[0].author_name,
+      year: result.docs[0].first_publish_year,
     }
   }
-};
+}
